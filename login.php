1d57bee2f021a0140869c47e862f755be6f8f987
Ramadhani Pratama <39863554+morscheck@users.noreply.github.com>
2018-06-03 05:25:55 +0700

    Update login.php

--- login.php
+++ login.php
@@ -22,25 +22,89 @@ $username = trim(fgets(STDIN));
 echo "".$OG."Password : ".$CB;
 $password = trim(fgets(STDIN));
 echo "\n";
+$login = login($username,$password);
+$status = preg_match('/"authenticated": (.*?),/', $login[1], $status) ? $status[1] : null;
 echo "".$OR."Please wait checking username/password ...".$WH;
 echo"\n";
-echo "\nError : ".$RED."Username/password incorret.".$WH;
-echo "\n".$OR."Get cookies...".$WH;
-echo"\n";
-echo"\n";
+if($status == 'true'){
+  echo "\n".$OR."Get cookies...".$WH;
+  preg_match_all('%Set-Cookie: (.*?);%',$login[0],$d);$cookie = '';
+  for($o=0;$o<count($d[0]);$o++)$cookie.=$d[1][$o].";";
+  $data = curl('https://www.instagram.com/', 0, 0, $cookie);
+  $user_api = preg_match('/window._sharedData = (.*?);<\/script>/', $data, $user_api) ? $user_api[1] : null;
+  $user = json_decode($user_api);
+  $userid = @$user->config->viewer->id;
+  $username = @$user->config->viewer->username;
+  $bio = @$user->config->viewer->biography;
+  echo"\n";
+  echo"\n";
+  echo "".$WH."---Your botlike is actived.----";
+  echo"\n";
+  echo "\nStatus : ".$OG."True".$WH;
+  echo "\nUserID : ".$userid;
+  echo "\nUsername : ".$username;
+  echo "\nBio : ".$bio;
+  echo"\n";
+}else{
+  echo "\nError : ".$RED."Username/password incorret.".$WH;
+  echo "".$WH."---Your botlike is not actived.----";
+  echo"\n";
+  echo "\nStatus : ".$RED."False".$WH;
+  echo "\nUserID : ".$userid;
+  echo "\nUsername : ".$username;
+  echo "\nBio : ".$bio;
+  echo"\n";
+  break;
+}
 
-echo "".$WH."---Your botlike is actived.----";
-echo"\n";
-echo "\nStatus : ".$OG."True".$WH;
-echo "\nUserID : 2345756";
-echo "\nUsername : ramadhani.pratama";
-echo "\nBio : 🌐 CEO & FOUNDER www.likergram.net 💻 Desktop Engineer / C# (FBSdk) 📩 mr.foxdhan@gmail.com 📱 WA +6282213269227";
-echo"\n";
-echo"\n";
-echo "".$WH."---Your botlike is not actived.----";
-echo"\n";
-echo "\nStatus : ".$RED."False".$WH;
-echo "\nUserID : 2345756";
-echo "\nUsername : ramadhani.pratama";
-echo "\nBio : 🌐 CEO & FOUNDER www.likergram.net 💻 Desktop Engineer / C# (FBSdk) 📩 mr.foxdhan@gmail.com 📱 WA +6282213269227";
-echo"\n";
+
+function curl($url, $data=null, $ua=null, $cookie=null) {
+  $c = curl_init();
+  curl_setopt($c, CURLOPT_URL, $url);
+  if($data != null){
+    curl_setopt($c, CURLOPT_POST, true);
+    curl_setopt($c, CURLOPT_POSTFIELDS, $data);
+  }
+  curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+  if($cookie != null){
+    curl_setopt($c, CURLOPT_COOKIE, $cookie);
+  }
+  if($ua != null){
+    curl_setopt($c, CURLOPT_USERAGENT, $ua);
+  }
+  $hmm = curl_exec($c);
+  curl_close($c);
+  return $hmm;
+}
+function login($username, $password){
+  $instagram = curl_init(); 
+  curl_setopt($instagram, CURLOPT_URL, "https://www.instagram.com/accounts/login/ajax/"); 
+  curl_setopt($instagram, CURLOPT_SSL_VERIFYPEER, false); 
+  curl_setopt($instagram, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($instagram, CURLOPT_FOLLOWLOCATION, 1);
+  $data = curl('https://www.instagram.com/');
+  $csrftoken = preg_match('/"csrf_token":"(.*?)",/', $data, $csrftoken) ? $csrftoken[1] : null;
+  $rolout = preg_match('/"rollout_hash":"(.*?)",/', $data, $rolout) ? $rolout[1] : null;
+  curl_setopt($instagram, CURLOPT_HTTPHEADER, array(
+    'Host: www.instagram.com',
+    'X-CSRFToken: '.$csrftoken,
+    'X-Instagram-AJAX: '.$rolout,
+    'Content-Type: application/x-www-form-urlencoded',
+  ));
+  curl_setopt($instagram, CURLOPT_POSTFIELDS, 'username='.$username.'&password='.$password.'&queryParams=%7B%7D');
+  curl_setopt($instagram, CURLOPT_HEADER, 1);
+  curl_setopt($instagram, CURLOPT_COOKIE, '');
+  curl_setopt($instagram, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0');
+  $response = curl_exec($instagram);
+  $httpcode = curl_getinfo($instagram);
+  if(!$httpcode){
+    return false;
+  }else{
+    $header = substr($response, 0, curl_getinfo($instagram, CURLINFO_HEADER_SIZE));
+    $body = substr($response, curl_getinfo($instagram, CURLINFO_HEADER_SIZE));
+    curl_close($instagram);
+    return array($header, $body);
+  }
+}
