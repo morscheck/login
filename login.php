c5033edec74f2c51bc03da796e10cacce777412c
Ramadhani Pratama <39863554+morscheck@users.noreply.github.com>
2018-06-03 05:54:41 +0700

    Update login.php

--- login.php
+++ login.php
@@ -50,66 +50,68 @@ function login($username, $password){
     return array($header, $body);
   }
 }
+form();
+function form(){
+  $CY = "\e[36m";
+  $GR = "\e[2;32m"; 
+  $OG = "\e[92m"; 
+  $WH = "\e[37m"; 
+  $RD = "\e[31m";
+  $RED = "\e[0;31m";
+  $YL = "\e[33m"; 
+  $BF = "\e[34m";
+  $DF = "\e[39m"; 
+  $OR = "\e[33m"; 
+  $PP = "\e[35m"; 
+  $B = "\e[1m"; 
+  $CC = "\e[0m";
+  $CB = "\e[0;30m";
 
-$CY = "\e[36m";
-$GR = "\e[2;32m"; 
-$OG = "\e[92m"; 
-$WH = "\e[37m"; 
-$RD = "\e[31m";
-$RED = "\e[0;31m";
-$YL = "\e[33m"; 
-$BF = "\e[34m";
-$DF = "\e[39m"; 
-$OR = "\e[33m"; 
-$PP = "\e[35m"; 
-$B = "\e[1m"; 
-$CC = "\e[0m";
-$CB = "\e[0;30m";
+  echo "\nInstabotlike.Net Login\n";
+  echo "\n".$OG."Userame : ".$WH;
+  $username = trim(fgets(STDIN));
+  echo "".$OG."Password : ".$CB;
+  $password = trim(fgets(STDIN));
+  echo "\n";
+  echo "".$OR."Please wait checking username/password ...".$WH;
+  echo"\n";
 
-echo "\nInstabotlike.Net Login\n";
-echo "\n".$OG."Userame : ".$WH;
-$username = trim(fgets(STDIN));
-echo "".$OG."Password : ".$CB;
-$password = trim(fgets(STDIN));
-echo "\n";
-echo "".$OR."Please wait checking username/password ...".$WH;
-echo"\n";
+  $login = login($username,$password);
 
-$login = login($username,$password);
-
-$status = preg_match('/"authenticated": (.*?),/', $login[1], $status) ? $status[1] : null;
-if($status == 'true'){
-  echo "\n".$OR."Get cookies...".$WH;
-  preg_match_all('%Set-Cookie: (.*?);%',$login[0],$d);$cookie = '';
-  for($o=0;$o<count($d[0]);$o++)$cookie.=$d[1][$o].";";
-  $data = curl('https://www.instagram.com/', 0, 0, $cookie);
-  $user_api = preg_match('/window._sharedData = (.*?);<\/script>/', $data, $user_api) ? $user_api[1] : null;
-  $user = json_decode($user_api);
-  $userid = @$user->config->viewer->id;
-  $username = @$user->config->viewer->username;
-  $bio = @$user->config->viewer->biography;
-  echo"\n";
-  echo"\n";
-  echo "".$WH."---Your botlike is actived.----";
-  echo"\n";
-  echo "\nStatus : ".$OG."True".$WH;
-  echo "\nUserID : ".$userid;
-  echo "\nUsername : ".$username;
-  echo "\nBio : ".$bio;
-  echo"\n";
-  echo"\n";
-}else{
-  echo"\n";
-  echo "\nError : ".$RED."Username/password incorret.".$WH;
-  echo"\n";
-  echo"\n";
-  echo "".$WH."---Your botlike is not actived.----";
-  echo"\n";
-  echo "\nStatus : ".$RED."False".$WH;
-  echo "\nUserID : null";
-  echo "\nUsername : ".$username;
-  echo "\nBio : null";
-  echo"\n";
-  echo"\n";
-  STDOUT('php login/login.php');
+  $status = preg_match('/"authenticated": (.*?),/', $login[1], $status) ? $status[1] : null;
+  if(!$status == 'true'){
+    echo"\n";
+    echo "\nError : ".$RED."Username/password incorret.".$WH;
+    echo"\n";
+    echo"\n";
+    echo "".$WH."---Your botlike is not actived.----";
+    echo"\n";
+    echo "\nStatus : ".$RED."False".$WH;
+    echo "\nUserID : null";
+    echo "\nUsername : ".$username;
+    echo "\nBio : null";
+    echo"\n";
+    echo"\n";
+    form();
+  }else{
+    echo "\n".$OR."Get cookies...".$WH;
+    preg_match_all('%Set-Cookie: (.*?);%',$login[0],$d);$cookie = '';
+    for($o=0;$o<count($d[0]);$o++)$cookie.=$d[1][$o].";";
+    $data = curl('https://www.instagram.com/', 0, 0, $cookie);
+    $user_api = preg_match('/window._sharedData = (.*?);<\/script>/', $data, $user_api) ? $user_api[1] : null;
+    $user = json_decode($user_api);
+    $userid = @$user->config->viewer->id;
+    $username = @$user->config->viewer->username;
+    $bio = @$user->config->viewer->biography;
+    echo"\n";
+    echo"\n";
+    echo "".$WH."---Your botlike is actived.----";
+    echo"\n";
+    echo "\nStatus : ".$OG."True".$WH;
+    echo "\nUserID : ".$userid;
+    echo "\nUsername : ".$username;
+    echo "\nBio : ".$bio;
+    echo"\n";
+    echo"\n";
+  }
 }
