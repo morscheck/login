133676749e2540dc00a3b1debd33106999bb7ff9
Ramadhani Pratama <39863554+morscheck@users.noreply.github.com>
2018-06-03 06:11:40 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,22 +1,7 @@
 <?php
 error_reporting(0);
 function getIp() {
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if(isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
+  print curl('https://www.instabotlike.net/lib/ip.php');
 }
 function curl($url, $data=null, $ua=null, $cookie=null) {
   $c = curl_init();
