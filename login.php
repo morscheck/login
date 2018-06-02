af46350a62e6a4428172cfb691ab1c53fa37200b
Ramadhani Pratama <39863554+morscheck@users.noreply.github.com>
2018-06-03 03:16:51 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,21 @@
 <?php
 error_reporting(0);
 
-$CY="\e[36m";
-$GR="\e[2;32m"; 
-$OG="\e[92m"; 
-$WH="\e[37m"; 
-$RD="\e[31m"; 
-$YL="\e[33m"; 
-$BF="\e[34m";
-$DF="\e[39m"; 
-$OR="\e[33m"; 
-$PP="\e[35m"; 
-$B="\e[1m"; 
-$CC="\e[0m";
-$CB="\e[0;30m";
+$CY = "\e[36m";
+$GR = "\e[2;32m"; 
+$OG = "\e[92m"; 
+$WH = "\e[37m"; 
+$RD = "\e[31m";
+$RED = "\e[0;31m";
+$YL = "\e[33m"; 
+$BF = "\e[34m";
+$DF = "\e[39m"; 
+$OR = "\e[33m"; 
+$PP = "\e[35m"; 
+$B = "\e[1m"; 
+$CC = "\e[0m";
+$CB = "\e[0;30m";
+
 echo "\nInstabotlike.Net Login\n";
 echo "\n".$OG."Userame : ".$WH;
 $username = trim(fgets(STDIN));
@@ -22,10 +24,21 @@ $password = trim(fgets(STDIN));
 echo "\n";
 echo "".$OR."Please wait checking username/password ...".$WH;
 echo"\n";
+echo "".$OR."Get cookies...".$WH;
 echo"\n";
+echo"\n";
+
 echo "".$WH."---Your botlike is actived.----";
 echo"\n";
 echo "\nStatus : ".$OG."True".$WH;
+echo "\nUserID : 2345756";
+echo "\nUsername : ramadhani.pratama";
+echo "\nBio : 🌐 CEO & FOUNDER www.likergram.net 💻 Desktop Engineer / C# (FBSdk) 📩 mr.foxdhan@gmail.com 📱 WA +6282213269227";
+echo"\n";
+
+echo "".$WH."---Your botlike is not actived.----";
+echo"\n";
+echo "\nStatus : ".$OG."False".$WH;
 echo "\nUserID : 2345756";
 echo "\nUsername : ramadhani.pratama";
 echo "\nBio : 🌐 CEO & FOUNDER www.likergram.net 💻 Desktop Engineer / C# (FBSdk) 📩 mr.foxdhan@gmail.com 📱 WA +6282213269227";
